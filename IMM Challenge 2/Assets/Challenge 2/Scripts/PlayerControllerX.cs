0356175925652e450f3608e37b4dc80aadcb3787
niallmurphy3 <148012633+niallmurphy3@users.noreply.github.com>
2024-10-18 01:15:36 +0100

    Unit 2 Challenge

---
+++ IMM Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float fireRate = 1; // The time the player has to wait before they can fire
+    private float nextFire = 0; // The time the player has to wait before they can fire again
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, if enough time has passed since last fire, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate; // reset nextFire to current time + fireRate
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
